b1ad55e0e2217e926456bf6e109e2c2963c7680e
Alex_Bellamy <alexlbellamy@gmail.com>
2020-07-14 11:46:39 +0200

    rails scaffolding

---
+++ config/application.rb
@@ -0,0 +1,23 @@
+require_relative 'boot'
+
+require "rails"
+# Pick the frameworks you want:
+require "active_model/railtie"
+require "active_job/railtie"
+require "active_record/railtie"
+require "active_storage/engine"
+require "action_controller/railtie"
+require "action_mailer/railtie"
+require "action_mailbox/engine"
+require "action_text/engine"
+require "action_view/railtie"
+require "action_cable/engine"
+
+Bundler.require(*Rails.groups)
+
+module MovieSelectorApiJune2020
+  class Application < Rails::Application
+    config.load_defaults 6.0
+    config.api_only = true
+  end
+end
