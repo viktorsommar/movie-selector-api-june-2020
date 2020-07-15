2f045ba2f2fc1dcd0413948a9d4625f4a3925908
viktor_sommar <vsommarbaillot@gmail.com>
2020-07-15 22:27:30 +0200

    Formatting and testing other ways of generating lists from api.

--- spec/requests/api/v0/movies_collection_endpoint_spec.rb
+++ spec/requests/api/v0/movies_collection_endpoint_spec.rb
@@ -1,22 +1,20 @@
-
-
 RSpec.describe 'Get /api/v0/movies', type: :request do
-describe 'successfully' do
+  describe 'successfully' do
     before do 
-        get '/api/v0/movies',
-        params: {
-            query: 'Batman'
-        }
+      get '/api/v0/movies',
+      params: {
+        query: 'Batman'
+      }
 
-        @response_json = JSON.parse(response.body)
+      @response_json = JSON.parse(response.body)
     end
 
     it 'is expected to return a collection of movies in an array' do 
-        expect(@response_json["results"]).to be_an Array
+      expect(@response_json["results"]).to be_an Array
     end
 
     it 'is expected to return a batman movie' do
-        expect(@response_json["results"].first["title"]).to eq 'Batman'
-end
-end
+      expect(@response_json["results"].first["title"]).to eq 'Batman'
+    end
+  end
 end
