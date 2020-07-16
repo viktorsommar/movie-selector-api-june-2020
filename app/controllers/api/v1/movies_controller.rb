a12159394b0c647dbc53c6651ed3d8c00a659a87
viktor_sommar <vsommarbaillot@gmail.com>
2020-07-16 17:15:41 +0200

    Changing the rand logic to better reflect the test

--- app/controllers/api/v1/movies_controller.rb
+++ app/controllers/api/v1/movies_controller.rb
@@ -4,7 +4,8 @@ class Api::V1::MoviesController < ApplicationController
     api_endpoint = "https://api.themoviedb.org/3/discover/movie?api_key=80089a9e6edb5f524156c569cd8a9a69&page=#{random_page}"
     results = RestClient.get(api_endpoint)
     movies = JSON.parse(results.body)["results"]
-    random_movie = movies[rand(0..(movies.count))]
+    binding.pry
+    random_movie = movies[rand(0..((movies.count)-1))]
     render json: {movie: random_movie}
   end
 end
