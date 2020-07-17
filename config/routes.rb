70772294076bfe3b221e8c7fc38ed7db68fb0037
Alex_Bellamy <alexlbellamy@gmail.com>
2020-07-17 10:15:05 +0200

    amending formatting in routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
-    namespace :api do
-        namespace :v1 do
-            resources :movies, only: [:show]
-        end
+  namespace :api do
+    namespace :v1 do
+      resources :movies, only: [:show]
     end
+  end
 end
