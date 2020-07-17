224b79f804ed93bfefa7b8b5569c14ef66ccaf0a
odeane_croney <odeane_croney@yahoo.com>
2020-07-17 14:52:18 +0200

    addig req test for subscriber

---
+++ spec/requests/api/v1/subscriber_can_add_movie_to_watchlist_spec.rb
@@ -0,0 +1,26 @@
+RSpec.describe 'POST /api/v1/watchlist_items', type: :request do
+  let(:subscriber) { create(:user, subscriber: true, email: 'subscriber@mail.com')}
+  let(:subscriber_credentials) { subscriber.create_new_auth_token }
+  let(:subscriber_headers) { {HTTP_ACCEPT: 'application/json'}.merge!(subscriber_credentials)}
+
+  
+  describe 'successfully' do
+    before do 
+      get '/api/v1/watchlist_items',
+      params: {
+        movie_db_id: 699,
+        title: 'For Your Eyes Only'
+      },
+      headers: subscriber_headers
+    end
+
+    it ' returns 200 response status' do
+      expect(response.status).to eq 200
+    end
+
+    it 'returns success message' do
+      expect(JSON.parse(response.body)["message"]).to eq "The movie has been added to your watchlist"
+    end
+
+  end
+end
