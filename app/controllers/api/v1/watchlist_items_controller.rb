2bdcabcedfa5bed7f04b604d14f3b89f23fb5bfc
Alex_Bellamy <alexlbellamy@gmail.com>
2020-07-18 13:49:42 +0200

    adding conditional statement - if user logged in can add to watch list, adding specs to test the sad path in this respect

--- app/controllers/api/v1/watchlist_items_controller.rb
+++ app/controllers/api/v1/watchlist_items_controller.rb
@@ -2,7 +2,8 @@ class Api::V1::WatchlistItemsController < ApplicationController
   before_action :authenticate_user!
 
   def create
-     
+     if current_user.subscriber
+
       if current_user.watchlist.nil?
         watchlist = Watchlist.create(user_id: current_user.id)
       else
@@ -11,5 +12,9 @@ class Api::V1::WatchlistItemsController < ApplicationController
     
     watchlist.watchlist_items.create(title: params["title"], movie_db_id: params["movie_db_id"])
     render json: { message: 'The movie has been added to your watchlist'}
+
+     else
+      render json: { message: "You need to become a subscriber before you can add anything to your watchlist" }, status: 401
+     end
   end
 end
--- spec/factories/watchlists.rb
+++ spec/factories/watchlists.rb
@@ -1,4 +1,5 @@
 FactoryBot.define do
   factory :watchlist do
+    user
   end
 end
