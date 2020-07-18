9915efced4c87df533500cf9ecc1a5c124042802
Alex_Bellamy <alexlbellamy@gmail.com>
2020-07-18 16:12:50 +0200

    amending as requested in pull request comments

--- app/controllers/api/v1/watchlist_items_controller.rb
+++ app/controllers/api/v1/watchlist_items_controller.rb
@@ -7,8 +7,8 @@ class Api::V1::WatchlistItemsController < ApplicationController
       else
         watchlist = current_user.watchlist
       end
-        watchlist.watchlist_items.create(title: params["title"], movie_db_id: params["movie_db_id"])
-        render json: { message: 'The movie has been added to your watchlist'}
+      watchlist.watchlist_items.create(title: params["title"], movie_db_id: params["movie_db_id"])
+      render json: { message: 'The movie has been added to your watchlist'}
       else
         render json: { message: "You need to become a subscriber before you can add anything to your watchlist" }, status: 401
     end
