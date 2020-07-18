c1a299780098767fc3d38c3607a45bcd8c0ffbe9
odeane_croney <odeane_croney@yahoo.com>
2020-07-18 20:55:33 +0200

    formatting

--- app/controllers/api/v1/subscriptions_controller.rb
+++ app/controllers/api/v1/subscriptions_controller.rb
@@ -1,16 +1,13 @@
 class Api::V1::SubscriptionsController < ApplicationController
   before_action :authenticate_user!
-  
   def create
     payment_status = perform_stripe_payment
-
     if payment_status[:paid] == true
       current_user.update_attribute(:subscriber, true)
       render json: { paid: true, message: "Successful payment, you are now a subscriber." }
     else
-      binding.pry
+      render json: { paid: false, message: "Payment did not go through"}, status: 400
     end
-
   end
 
 private
@@ -21,13 +18,11 @@ private
       source: params[:stripeToken],
       description: 'Movie Selector'
     )
-
     charge = Stripe::Charge.create(
       customer: customer.id,
       amount: 29,
       currency: 'sek'
     )
-
     charge 
   end
 end
--- config/application.rb
+++ config/application.rb
@@ -1,5 +1,4 @@
 require_relative 'boot'
-
 require 'rails'
 require 'active_model/railtie'
 require 'active_job/railtie'
