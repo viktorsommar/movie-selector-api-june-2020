a064ae8be1ecc047c0d285a7da416f88b08f4c5b
odeane_croney <odeane_croney@yahoo.com>
2020-07-21 09:15:13 +0200

    formatting

--- app/controllers/api/v1/watchlist_items_controller.rb
+++ app/controllers/api/v1/watchlist_items_controller.rb
@@ -3,13 +3,10 @@ class Api::V1::WatchlistItemsController < ApplicationController
  
   def create
     if current_user.subscriber
-      
       watchlist = current_user.watchlist
       watchlist.watchlist_items.create(title: params["title"], movie_db_id: params["movie_db_id"])
-
       json = { watchlist: WatchlistSerializer.new(watchlist) }
       json.merge!(message: 'The movie has been added to your watchlist')
-
       render json: json
       else
       render json: { message: "You need to become a subscriber before you can add anything to your watchlist" }, status: 401
